6d1a365b8d7f609ea4422e0e902e9ad9eadc9081
Jakub Bracha <jakub.bracha@gmail.com>
2022-12-23 13:16:21 +0100

    Make EcbCurrencyProvider async

--- CurrencyConverter/ICurrencyRateProviderAsync.cs
+++ CurrencyConverter/ICurrencyRateProviderAsync.cs
@@ -8,8 +8,14 @@ namespace Jakubqwe.CurrencyConverter
 {
     internal interface ICurrencyRateProviderAsync : ICurrencyRateProvider
     {
+        /// <summary>
+        ///     Gets the conversion rate for given date asynchronously
+        /// </summary>
         Task<decimal> GetRateAsync(Currency sourceCurrency, Currency targetCurrency, DateTime date);
 
+        /// <summary>
+        ///     Gets the latest conversion rate asynchronously
+        /// </summary>
         Task<decimal> GetRateAsync(Currency sourceCurrency, Currency targetCurrency);
     }
 }
