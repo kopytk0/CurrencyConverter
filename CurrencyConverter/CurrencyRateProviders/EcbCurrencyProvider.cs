922104fd7481eb51023313c48ed46060fd3aa6de
Jakub Bracha <jakub.bracha@gmail.com>
2022-12-23 13:16:58 +0100

    cleanup

--- CurrencyConverter/CurrencyRateProviders/EcbCurrencyProvider.cs
+++ CurrencyConverter/CurrencyRateProviders/EcbCurrencyProvider.cs
@@ -4,7 +4,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using CsvHelper;
